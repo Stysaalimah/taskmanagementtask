f4ab58e30633d8cdcb4f6c0556ad3b9f9639a38d
sitysalimah8@gmail.com <sitysalimah8@gmail.com>
2022-11-17 13:32:48 +0700

    commit tugas 4

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -9,16 +11,83 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: Colors.blue[100],
+      body: Row(children: [
+        Expanded(
+          flex: 2,
+          child: Container(
+            height: Get.height,
+            color: Colors.blue[100],
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 100,
+                    width: double.infinity,
+                    alignment: Alignment.topCenter,
+                    child: const Image(
+                      image: AssetImage('assets/icons/icon.png'),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 100,
+                    child: Center(
+                      child: InkWell(
+                        child: Column(children: [
+                          Container(
+                            height: 40,
+                            width: 100,
+                            decoration: Get.currentRoute == '/home'
+                                ? BoxDecoration(
+                                    borderRadius: BorderRadius.circular(40),
+                                    color: Colors.white,
+                                  )
+                                : BoxDecoration(),
+                            child: Icon(
+                                Get.currentRoute == '/home'
+                                    ? Ionicons.desktop
+                                    : Ionicons.desktop_outline,
+                                color: Colors.grey,
+                                size: 30),
+                          ),
+                          SizedBox(
+                            height: 100,
+                            child: Center(
+                              child: InkWell(
+                                child: Column(children: [
+                                  Container(
+                                    height: 40,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(40),
+                                      color: Colors.white,
+                                    ),
+                                    child: Icon(Ionicons.desktop,
+                                        color: Colors.grey, size: 30),
+                                  ),
+
+                                  // ignore: prefer_const_constructors
+
+                                  const Text(
+                                    'Home',
+                                    style: TextStyle(
+                                        color: Colors.grey, fontSize: 16),
+                                  ),
+                                ]),
+                              ),
+                            ),
+                          ),
+                        ]),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),
+      ]),
     );
   }
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:taskmanagementtask/app/routes/app_pages.dart';
 
 import '../controllers/login_controller.dart';
 
@@ -9,15 +10,109 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: Colors.blue[100],
+      body: Container(
+        margin: context.isPhone
+            ? EdgeInsets.all(Get.width * 0.1)
+            : EdgeInsets.all(Get.height * 0.1),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.white,
         ),
+        child: Row(children: [
+//biru
+          !context.isPhone
+              ? Expanded(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        bottomLeft: Radius.circular(50),
+                      ),
+                      color: Colors.blue,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            const Text(
+                              'Welcome',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 70,
+                              ),
+                            ),
+                            const Text(
+                              'Please Sign In',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                              ),
+                            ),
+                            const Text(
+                              'Star Journey With Us',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ]),
+                    ),
+                  ),
+                )
+              : const SizedBox(),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                ),
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    context.isPhone
+                        ? Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            // ignore: prefer_const_literals_to_create_immutables
+                            children: [
+                                const Text(
+                                  'Welcome',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 40,
+                                  ),
+                                ),
+                                const Text(
+                                  'Please Sign In',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                const Text(
+                                  'Star Journey With Us',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ])
+                        : SizedBox(),
+                    Image.asset('assets/images/login.png'),
+                    FloatingActionButton.extended(
+                      onPressed: () => Get.toNamed(Routes.HOME),
+                      label: const Text('Sign In With Google'),
+                    ),
+                  ]),
+            ),
+          ),
+        ]),
       ),
     );
   }
