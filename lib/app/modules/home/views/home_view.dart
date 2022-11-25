c9ce8edc591c6c0ec64eb81cfeba3e6efa526345
sitysalimah8@gmail.com <sitysalimah8@gmail.com>
2022-11-25 10:08:13 +0700

    comit

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,91 +1,188 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:get/get.dart';
+import 'package:taskmanagementtask/app/utils/style/AppColors.dart';
+import 'package:taskmanagementtask/main.dart';
 
+import '../../../utils/widget/header.dart';
+import '../../../utils/widget/sideBar.dart/side.Bar.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[100],
+      key: _drawerKey,
+      drawer: const SideBar(),
+      backgroundColor: AppColors.primaryBg,
       body: Row(children: [
+        !context.isPhone
+            ? const Expanded(
+                flex: 2,
+                child: SideBar(),
+              )
+            : const SizedBox(),
         Expanded(
-          flex: 2,
-          child: Container(
-            height: Get.height,
-            color: Colors.blue[100],
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 100,
-                    width: double.infinity,
-                    alignment: Alignment.topCenter,
-                    child: const Image(
-                      image: AssetImage('assets/icons/icon.png'),
+          flex: 15,
+          child: Column(children: [
+            !context.isPhone
+                ? const header()
+                : Container(
+                    padding: const EdgeInsets.all(20),
+                    child: Row(children: [
+                      IconButton(
+                        onPressed: () {
+                          _drawerKey.currentState!.openDrawer();
+                        },
+                        icon: const Icon(
+                          Icons.menu,
+                          color: AppColors.primaryText,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Column(
+                          // mainAxisAlignment: MainAxisAlignment.start
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            const Text(
+                              'Task Management',
+                              style: TextStyle(
+                                  fontSize: 20, color: AppColors.primaryText),
+                            ),
+                            const Text(
+                              'Manage task made easy with friends',
+                              style: TextStyle(
+                                  fontSize: 15, color: AppColors.primaryText),
+                            ),
+                          ]),
+                      const Spacer(),
+                      const Icon(
+                        FontAwesomeIcons.bell,
+                        color: AppColors.primaryText,
+                        size: 30,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: const CircleAvatar(
+                          backgroundColor: Colors.amber,
+                          radius: 25,
+                          foregroundImage: NetworkImage(
+                              'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/27/08/jennifer-lawrence.jpg'),
+                        ),
+                      ),
+                    ]),
+                  ),
+            //content / isi page / screen
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(50),
+                margin: !context.isPhone
+                    ? const EdgeInsets.all(10)
+                    : const EdgeInsets.all(0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: !context.isPhone
+                      ? BorderRadius.circular(50)
+                      : BorderRadius.circular(30),
+                ),
+                // ignore: prefer_const_literals_to_create_immutables
+                child: Column(children: [
+                  const Text(
+                    'My Task',
+                    style: TextStyle(
+                      color: AppColors.primaryText,
+                      fontSize: 30,
                     ),
+                  ),
+                  const SizedBox(
+                    height: 20,
                   ),
                   SizedBox(
-                    height: 100,
-                    child: Center(
-                      child: InkWell(
-                        child: Column(children: [
+                    height: 200,
+                    child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        children: [
                           Container(
-                            height: 40,
-                            width: 100,
-                            decoration: Get.currentRoute == '/home'
-                                ? BoxDecoration(
-                                    borderRadius: BorderRadius.circular(40),
-                                    color: Colors.white,
-                                  )
-                                : BoxDecoration(),
-                            child: Icon(
-                                Get.currentRoute == '/home'
-                                    ? Ionicons.desktop
-                                    : Ionicons.desktop_outline,
-                                color: Colors.grey,
-                                size: 30),
-                          ),
-                          SizedBox(
-                            height: 100,
-                            child: Center(
-                              child: InkWell(
-                                child: Column(children: [
-                                  Container(
-                                    height: 40,
-                                    width: 100,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(40),
-                                      color: Colors.white,
+                            width: 400,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: AppColors.cardBg,
+                            ),
+                            padding: EdgeInsets.all(20),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(25),
+                                      child: const CircleAvatar(
+                                        backgroundColor: Colors.amber,
+                                        radius: 20,
+                                        foregroundImage: NetworkImage(
+                                            'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/27/08/jennifer-lawrence.jpg'),
+                                      ),
                                     ),
-                                    child: Icon(Ionicons.desktop,
-                                        color: Colors.grey, size: 30),
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(25),
+                                      child: const CircleAvatar(
+                                        backgroundColor: Colors.amber,
+                                        radius: 20,
+                                        foregroundImage: NetworkImage(
+                                            'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/27/08/jennifer-lawrence.jpg'),
+                                      ),
+                                    ),
+                                    const Spacer(),
+                                    Container(
+                                      height: 25,
+                                      width: 80,
+                                      color: AppColors.primaryBg,
+                                      child: const Center(
+                                        child: Text('100 %'),
+                                      ),
+                                    ),
+                                  ]),
+                                  const Spacer(),
+                                  Container(
+                                    height: 25,
+                                    width: 80,
+                                    color: AppColors.primaryBg,
+                                    child: const Center(
+                                      child: Text('10 / 10  Task'),
+                                    ),
                                   ),
-
-                                  // ignore: prefer_const_constructors
-
                                   const Text(
-                                    'Home',
+                                    'Pemrograman Mobile',
                                     style: TextStyle(
-                                        color: Colors.grey, fontSize: 16),
+                                      color: AppColors.primaryText,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  const Text(
+                                    'Deadline 2 hari Lagi',
+                                    style: TextStyle(
+                                      color: AppColors.primaryText,
+                                      fontSize: 15,
+                                    ),
                                   ),
                                 ]),
-                              ),
-                            ),
                           ),
                         ]),
-                      ),
-                    ),
                   ),
-                ],
+                ]),
               ),
             ),
-          ),
+          ]),
         ),
       ]),
     );
--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,24 +1,110 @@
 import 'package:flutter/material.dart';
 
-import 'package:get/get.dart';
+import 'dart:html';
 
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:get/get.dart';
+import 'package:taskmanagementtask/app/utils/style/AppColors.dart';
+
+import '../../../utils/widget/header.dart';
+import '../../../utils/widget/sideBar.dart/side.Bar.dart';
+import '../../home/controllers/home_controller.dart';
 import '../controllers/profile_controller.dart';
 
 class ProfileView extends GetView<ProfileController> {
-  const ProfileView({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ProfileView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'ProfileView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        key: _drawerKey,
+        drawer: const SideBar(),
+        backgroundColor: AppColors.primaryBg,
+        body: Row(
+          children: [
+            !context.isPhone
+                ? const Expanded(
+                    flex: 2,
+                    child: SideBar(),
+                  )
+                : const SizedBox(),
+            Expanded(
+              flex: 15,
+              child: Column(children: [
+                !context.isPhone
+                    ? const header()
+                    : Container(
+                        padding: const EdgeInsets.all(20),
+                        child: Row(children: [
+                          IconButton(
+                            onPressed: () {
+                              _drawerKey.currentState!.openDrawer();
+                            },
+                            icon: const Icon(
+                              Icons.menu,
+                              color: AppColors.primaryText,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Column(
+                              // mainAxisAlignment: MainAxisAlignment.start
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                const Text(
+                                  'Task Management',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: AppColors.primaryText),
+                                ),
+                                const Text(
+                                  'Manage task made easy with friends',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: AppColors.primaryText),
+                                ),
+                              ]),
+                          const Spacer(),
+                          const Icon(
+                            FontAwesomeIcons.bell,
+                            color: AppColors.primaryText,
+                            size: 30,
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(30),
+                            child: const CircleAvatar(
+                              backgroundColor: Colors.amber,
+                              radius: 25,
+                              foregroundImage: NetworkImage(
+                                  'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/27/08/jennifer-lawrence.jpg'),
+                            ),
+                          ),
+                        ]),
+                      ),
+                //content / isi page / screen
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(50),
+                    margin: !context.isPhone
+                        ? const EdgeInsets.all(10)
+                        : const EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: !context.isPhone
+                          ? BorderRadius.circular(50)
+                          : BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+          ],
+        ));
   }
 }
